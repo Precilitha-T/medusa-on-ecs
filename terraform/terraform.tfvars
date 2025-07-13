d1aec86946ccb0f4b95b4929d7204b56ed8c30f5
preci <talasuprecilitha@gmail.com>
2025-07-14 00:31:03 +0530

    Initial commit with GitHub Actions

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region     = "us-east-2"
+ecr_image_url  = "871328969351.dkr.ecr.us-east-2.amazonaws.com/medusa-backend"
+
---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+variable "ecr_image_url" {
+  type        = string
+  description = "871328969351.dkr.ecr.us-east-2.amazonaws.com/medusa-backend"
+}
